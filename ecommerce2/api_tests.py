23a02ec08c12e62a950099f5191efbf31253a5d5
KazuhisaTaniguchi <jjzoow96@gmail.com>
2016-04-05 18:08:25 +0900

    changed

--- ecommerce2/api_tests.py
+++ ecommerce2/api_tests.py
@@ -11,7 +11,7 @@ cart_url = base_url + 'cart/'
 
 data = {
     'username': 'taniguchi',
-    'password': 'password',
+    'password': 'taniguchi96',
 }
 login_request = requests.post(login_url, data=data)
 
